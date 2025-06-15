f65c64695f9891dacfd32f3746152da112a667b3
Chris <chrisstudio1016@gmail.com>
2025-06-15 22:39:46 +0800

    web: playground mic listening

--- web/src/app/playground/components/chatroom-input.tsx
+++ web/src/app/playground/components/chatroom-input.tsx
@@ -1,24 +1,37 @@
+"use client";
 import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
 import { Textarea } from "@/components/ui/textarea";
 import { Mic, Paperclip, Send } from "lucide-react";
 
 export default function ChatroomInput() {
+  const startRecording = async () => {
+    const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
+    const mediaRecorder = new MediaRecorder(stream);
+    console.log(mediaRecorder);
+    const audioChunks = [];
+    mediaRecorder.addEventListener("dataavailable", (event) => {
+      audioChunks.push(event.data);
+      console.log(event.data);
+    });
+  };
   return (
     <div className="size-full flex flex-row items-center gap-5">
       <div className="relative h-full w-[94%]">
         <Textarea className="size-full" placeholder="Type your message here" />
         <div className="absolute left-3 bottom-1">
-          <Button variant="ghost" className="rounded-3xl">
+          <Button variant="ghost">
             <Paperclip />
           </Button>
-          <Button variant="ghost" className="rounded-3xl">
+          <Button
+            variant="ghost"
+            className="rounded-3xl"
+            onClick={startRecording}
+          >
             <Mic />
           </Button>
         </div>
       </div>
-      <Button className="h-[80%] w-[6%] rounded-4xl">
+      <Button className="h-[70%] w-[6%] rounded-4xl">
         <Send />
       </Button>
     </div>
