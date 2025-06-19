5b0bbf71e4bc50303e3a82671801f0a420f86443
Chris <chrisstudio1016@gmail.com>
2025-06-19 22:06:19 +0800

    web: fix upload multiple files too many render error

--- web/src/app/playground/components/uploaded-files.tsx
+++ web/src/app/playground/components/uploaded-files.tsx
@@ -16,11 +16,9 @@ const UploadedFileMessage = ({ files }: UploadedFileMessageProps) => {
         setPreviewUrl(event.target?.result as string);
       };
       reader.readAsDataURL(file);
-    } else {
-      setPreviewUrl(null);
     }
 
-    if (previewUrl) {
+    if (previewUrl && file.type.startsWith("image/")) {
       return (
         <div key={index} className="w-max self-end">
           <Image
@@ -34,19 +32,24 @@ const UploadedFileMessage = ({ files }: UploadedFileMessageProps) => {
       );
     }
 
-    return (
-      <div key={index} className="w-max self-end flex border-[1px] rounded-2xl">
-        <div className="size-full flex flex-row justify-center items-center gap-2 ml-2 mr-2">
-          <Image src={pdfLogo} alt="pdf-file" width={35} height={35} />
-          <div className="flex flex-col justify-center items-center">
-            <div>{file.name}</div>
-            <div className="text-[#A5A5A5]">
-              {(file.size / 1024 / 1024).toFixed(1)} MB
+    if (file.type === "application/pdf") {
+      return (
+        <div
+          key={index}
+          className="w-max self-end flex border-[1px] rounded-2xl"
+        >
+          <div className="size-full flex flex-row justify-center items-center gap-2 ml-2 mr-2">
+            <Image src={pdfLogo} alt="pdf-file" width={35} height={35} />
+            <div className="flex flex-col justify-center items-center">
+              <div>{file.name}</div>
+              <div className="text-[#A5A5A5]">
+                {(file.size / 1024 / 1024).toFixed(1)} MB
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    );
+      );
+    }
   });
 };
 
